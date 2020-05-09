6e46a2319e364e55ae9bd810c6fccb2c91a4199f
ZNevzz <dsouza.nevil45@gmail.com>
2020-05-09 09:31:02 +0530

    project

---
+++ src/main/java/spring/znevzz/reactive/EnableSpringCachingPatternApplication.java
@@ -0,0 +1,13 @@
+package spring.znevzz.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnableSpringCachingPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnableSpringCachingPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/znevzz/reactive/EnableSpringCachingPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.znevzz.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableSpringCachingPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
