ab8e0ad77ecbc3a18659e7a48263962543714aa1
ZNevzz <dsouza.nevil45@gmail.com>
2020-05-09 10:49:20 +0530

    Post implementation started

--- src/main/java/spring/znevzz/reactive/controller/CacheController.java
+++ src/main/java/spring/znevzz/reactive/controller/CacheController.java
@@ -1,7 +1,13 @@
 package spring.znevzz.reactive.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import spring.znevzz.reactive.bean.ICacheRequest;
+import spring.znevzz.reactive.bean.ICacheResponse;
+import spring.znevzz.reactive.bean.SimpleCacheRequest;
 
 @RestController
 public class CacheController {
@@ -10,4 +16,10 @@ public class CacheController {
     public String welcome() {
         return "welcome";
     }
+
+    @PostMapping("/cache")
+    public Flux<ICacheResponse> fromCache(@RequestBody SimpleCacheRequest request) {
+        return Flux.empty();
+    }
+
 }
